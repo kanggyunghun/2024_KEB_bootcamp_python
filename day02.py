c8bb9b6ec5a930a0b877207a3fac9e91918f04b9
kanggyunghun <kkh1574@inha.edu>
2024-01-15 15:47:50 +0900

    v12 if vowels

--- day02.py
+++ day02.py
@@ -1,14 +1,7 @@
-# (32°F − 32) × 5/9 = 0°C
-# (0°C × 9/5) + 32 = 32°F
-fahrenheit = float(input('Enter temperature in Fahrenheit:'))
-print(f'fahrenheit : {fahrenheit}F, Celsius : {round(((fahrenheit-32.0) *5.0/9.0),2)}C')
-print(f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0) *5.0/9.0):.2f}C')
-
-menu = input('1) Fehrenheit -> Celsius 2) Celsius -> Fehrenheit 3) Quit program :')
-
-if menu == '1':
-    fahrenheit = float(input('Enter temperature in Fahrenheit:'))
-    print(f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.2f}C')
-elif menu == '2':
-    celsius = float(input('Enter temperature in Celsius :'))
-    print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9/5) + 32):.2f}')
+letter = input('Enter alphabet :')
+#vowels = {'a','e','i','o','u'} # set
+vowels ='aeiou'
+if letter in vowels: # in
+    print(f'{letter} is a vowel')
+else :
+    print(f'{letter} is a consonant')
