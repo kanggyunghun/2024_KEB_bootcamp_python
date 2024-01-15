261ad1edcd2028d9030e218ddeb533c077171801
kanggyunghun <kkh1574@inha.edu>
2024-01-15 14:15:28 +0900

    v0.9 ASCII

--- day02.py
+++ day02.py
@@ -1,13 +1,8 @@
-base_number = int(input('input base number :'))
-exponent_number = int(input('input exponent number :'))
-
-print(f'base {base_number}, exponent {exponent_number}, 결과 값은:{base_number**exponent_number}')
-print('base {0}, exponent {1}, 결과 값은:{2}'.format(base_number, exponent_number,base_number**exponent_number))
-print('base %d, exponent %d, 결과 값은 %d' % (base_number, exponent_number,base_number**exponent_number))
-
-# money = 5,000,000 # 쉼표는 튜플로 동작한다.
-# print(money)
-# print(type(money)) # tuple
-# cash = 5_000_000
-# print(cash, type(cash))
-#
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'), ord('a'), ord('2'))
