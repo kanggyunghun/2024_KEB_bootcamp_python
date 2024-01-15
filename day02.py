d982c7e532330b286ffac6f0d4d2f306b7687694
kanggyunghun <kkh1574@inha.edu>
2024-01-15 13:21:49 +0900

    v0.7 python day02

--- day02.py
+++ day02.py
@@ -1,6 +1,10 @@
 base_number = int(input('input base number :'))
 exponent_number = int(input('input exponent number :'))
-print(f'base {base_number}, exponent {exponent_number}, 결과 값은{base_number**exponent_number}')
+
+print(f'base {base_number}, exponent {exponent_number}, 결과 값은:{base_number**exponent_number}')
+print('base {0}, exponent {1}, 결과 값은:{2}'.format(base_number, exponent_number,base_number**exponent_number))
+print('base %d, exponent %d, 결과 값은 %d' % (base_number, exponent_number,base_number**exponent_number))
+
 # money = 5,000,000 # 쉼표는 튜플로 동작한다.
 # print(money)
 # print(type(money)) # tuple
