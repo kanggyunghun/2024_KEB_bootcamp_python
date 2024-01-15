e01dd379e0d56e176fcc2c5bea93b2ed95831619
kanggyunghun <kkh1574@inha.edu>
2024-01-15 11:43:36 +0900

    v0.5 python day0.2

--- day02.py
+++ day02.py
@@ -10,3 +10,9 @@ print(abc,Abc,ABC)
 
 test9 = 77
 _test9 = 77
+
+aritists = ['bts','뉴진스','핑클','SES','HOT','블랙핑크']
+group = aritists
+print(aritists, group)
+aritists[2] = '세븐틴'
+print(aritists, group)
