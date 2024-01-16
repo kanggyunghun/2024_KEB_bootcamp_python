d8bf73a4fc90e358c0c16bea409bf46f1088b41c
kanggyunghun <kkh1574@inha.edu>
2024-01-16 14:40:31 +0900

    prime number 4

--- day03.py
+++ day03.py
@@ -3,15 +3,18 @@ number = int(input('enter number :'))
 is_prime = True
 i = 2
 
-while i < number:
-    if number % i == 0:
-        is_prime = False
-        break
-    i += 1
-
-if is_prime:
-    print(f'{number}는 prime number 입니다')
-else:
+if number < 2:
     print(f'{number}는 prime number가 아닙니다.')
+else:
+    while i < number:
+        if number % i == 0:
+            is_prime = False
+            break
+        i += 1
+
+    if is_prime:
+        print(f'{number}는 prime number 입니다')
+    else:
+        print(f'{number}는 prime number가 아닙니다.')
 
 
