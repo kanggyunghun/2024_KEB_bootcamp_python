3f5977969e70f35c1a9dd3d980ec9a00985853ad
kanggyunghun <kkh1574@inha.edu>
2024-01-16 14:37:41 +0900

    prime number 3

--- day03.py
+++ day03.py
@@ -1,17 +1,17 @@
-# prime number
-
+# prime number 2
 number = int(input('enter number :'))
-count = 0
+is_prime = True
 i = 2
 
-while i <= number:
+while i < number:
     if number % i == 0:
-        count = count + 1
+        is_prime = False
         break
-    i = i + 1
+    i += 1
 
-if count == 0:
+if is_prime:
     print(f'{number}는 prime number 입니다')
 else:
     print(f'{number}는 prime number가 아닙니다.')
+
 
