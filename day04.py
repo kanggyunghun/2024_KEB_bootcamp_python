b408cf5649265a6a5e7f66c147d2721e68f50a69
kanggyunghun <kkh1574@inha.edu>
2024-01-17 13:34:46 +0900

    deepcopy

--- day04.py
+++ day04.py
@@ -1,24 +1,9 @@
-
-numbers = input('enter number :').split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
-
-if n1 > n2:
-    n1, n2 = n2, n1
-
-for number in range(n1, n2 + 1):
-    is_prime = True
-
-    if number < 2:
-        pass
-    else:
-        i = 2
-        while i*i <= number: # performance issue
-            if number % i == 0:
-                is_prime = False
-                break
-            i = i+1
-            #print(i, end=' ') # loop count
-        if is_prime:
-            #pass
-            print(number, end=' ')
+import copy
+subjects = [1,2,[3,4],5]
+a = subjects
+b = a.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(subjects)
+subjects[2][1] = 2
+print(subjects ,a, b, c, d, e)
