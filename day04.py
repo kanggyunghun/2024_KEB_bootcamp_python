62251f1a27dd692d71a16221e295d6be2372dd08
kanggyunghun <kkh1574@inha.edu>
2024-01-17 09:23:35 +0900

    day04 first

--- day04.py
+++ day04.py
@@ -1,19 +1,56 @@
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+while True:
+    menu = input('1) Fahrenheit-> Celsius 2) Celsius -> Fahrenheit 3) Prime1 4) Prime2 5) Quit :')
 
-if n1 > n2:
-    n1, n2 = n2, n1
+    if menu == '1':
+        fahrenheit = int(input('Enter temperature in Fahrenheit:'))
+        print(f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
 
-for number in range(n1, n2+1):
-    is_prime = True
+    elif menu == '2':
+        celsius = int(input('Enter temperature in Celsius:'))
+        print(f'celsius : {celsius}F, Celsius : {((celsius * 9.0/5.0) + 32.0):.4f}F')
 
-    if number < 2:
-        #pass
-        continue
+    elif menu == '3':
+        number = int(input('enter number :'))
+        is_prime = True
+
+        if number < 2:
+            print(f'{number}는 prime number가 아닙니다.')
+        else:
+            for i in range(2, number):
+                if number % i == 0:
+                    is_prime = False
+                    break
+                i += 1
+
+            if is_prime:
+                print(f'{number}는 prime number 입니다')
+            else:
+                print(f'{number}는 prime number가 아닙니다.')
+
+    elif menu == '4':
+        numbers = input('enter number :').split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
+
+        if n1 > n2:
+            n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            is_prime = True
+
+            if number < 2:
+                pass
+            else:
+                for i in range(2, number):
+                    if number % i == 0:
+                        is_prime = False
+                        break
+                if is_prime: print(number, end=' ')
+
+    elif menu == '5':
+        pass
+
     else:
-        for i in range(2, number):
-            if number % i == 0:
-                is_prime = False
-                break
-        if is_prime: print(number, end=' ')
+        print('invalid Menu')
+
+
