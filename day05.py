424b0d3fcb420e88ebd76fe86be1004095cc9417
kanggyunghun <kkh1574@inha.edu>
2024-01-18 10:51:27 +0900

    day05 function

--- day05.py
+++ day05.py
@@ -1,5 +1,31 @@
-word = 'letter'
-letter_counts = {}
-for letter in word:
-    letter_counts[letter] = word.count(letter)
-print(letter_counts)
+def is_prime(num) -> bool: # help() 와 같이 사용
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param num: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """
+    if num < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= num:
+            if num % i == 0:
+                return False
+            i += 1
+        return True
+
+
+numbers = input('enter number :').split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2 + 1):
+    if is_prime(number):
+        print(number, end=' ')
+
+
+
+
