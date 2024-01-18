7b7898d544ff5085a529ae9dbd3d65ae263974be
kanggyunghun <kkh1574@inha.edu>
2024-01-18 14:24:45 +0900

    prime number using map function

--- day05.py
+++ day05.py
@@ -1,8 +1,14 @@
-def out_function(nout):
-    def inner_function():
-        return nout * nout
-    return inner_function
+n1, n2 = map(int,input('enter number :').split().sort())
+n1, n2 = min(n1, n2), max(n1, n2)
 
-a = out_function(1)
-b = out_function(2)
-print(a(),b())
+for number in range(n1, n2 + 1):
+    is_prime = True
+
+    if number < 2:
+        pass
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime: print(number, end=' ')
