4a2f99a08c315292ac51dac72dd284f5cd54de61
kanggyunghun <kkh1574@inha.edu>
2024-01-19 11:12:29 +0900

    raise exception

--- day06.py
+++ day06.py
@@ -1,17 +1,23 @@
 import random
 
-# numbers = []
-# for i in range(1,101):
-#     numbers.append(random.randint(1,100))
+class OopsException(Exception):
+    pass
+
 numbers = [random.randint(0,100) for i in range(10)]
 print(numbers)
 
 try:
     pick = int(input(f"Input index (0~{len(numbers)-1}): "))
     print(numbers[pick])
+    print(5/2)
+    raise OopsException('Oops~') # 강제 예외 발생 C++ 는 throw
 except IndexError:
     print("Out of range : wrong index number")
 except ValueError:
     print('Input Only Number~')
-except Exception:
-    print('Error occurs')
+except OopsException as err:
+    print(err)
+except Exception as err:
+    print(f'Error occurs{err}')
+else:
+    print(f'program terminate')
