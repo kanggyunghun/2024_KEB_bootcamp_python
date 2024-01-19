208f6c8da93dfa8c6517187f46546025e47f47a4
kanggyunghun <kkh1574@inha.edu>
2024-01-19 10:54:39 +0900

    try except

--- day06.py
+++ day06.py
@@ -1,25 +1,17 @@
-def factorial_repetition(n) -> int:
-    """
-    반복문을 이용한 팩토리얼 함수
-    :param n: int
-    :return: int
-    """
-    result = 1
-    for i in range(2, n+1):
-        result = result * i
-    return result
+import random
 
-def factorial_recursion(n):
-    """
-    재귀함수를 사용한 팩토리얼 함수
-    :param n:  int
-    :return: function, int
-    """
-    if n == 1:
-        return n
-    else:
-        return n*factorial_recursion(n-1)
+# numbers = []
+# for i in range(1,101):
+#     numbers.append(random.randint(1,100))
+numbers = [random.randint(0,100) for i in range(10)]
+print(numbers)
 
-number = int(input('number : '))
-print(factorial_repetition(number))
-print(factorial_recursion(number))
+try:
+    pick = int(input(f"Input index (0~{len(numbers)-1}): "))
+    print(numbers[pick])
+except IndexError:
+    print("Out of range : wrong index number")
+except ValueError:
+    print('Input Only Number~')
+except Exception:
+    print('Error occurs')
